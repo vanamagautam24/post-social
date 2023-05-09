a0d4bfa89119ec32d97a6d8da2b9a7b2831d2cc7
vanamagautam24 <gautamv@umd.edu>
2023-05-09 00:01:40 -0400

    add env variables

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-// server.js
 const app = require('./app');
 const pool = require('./pool');
 
+const {
+  DB_HOST = 'localhost',
+  DB_PORT = 5432,
+  DB_NAME = 'post-social',
+  DB_USER = 'vanama',
+  DB_PASSWORD = 'password',
+  PORT = 3005,
+} = process.env;
+
 pool
   .connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'post-social',
-    user: 'vanama',
-    password: 'password',
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
   })
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      // Remove the parentheses after 'app'
-      console.log('Server is listening on port 3005');
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
     });
   })
   .catch((err) => {
